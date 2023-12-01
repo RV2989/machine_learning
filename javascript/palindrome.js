fd6adc54bba45c0eeb404ae3092b94e9b065308b
RV2989 <rutujaverekar4@gmail.com>
2023-12-01 19:27:27 +0530

    commiting all my work uptil now

---
+++ javascript/palindrome.js
@@ -0,0 +1,1 @@
+let pal=promt("enter the ")
---
+++ javascript/variables.js
@@ -0,0 +1,12 @@
+// we can define variables using let var and const
+// the most efficient way of using is to use let for declaration of the variables
+// there are 7 premitive variables in java
+let a="rutuja"
+let b=23;
+let c=true;
+let d=BigInt("4566");
+let e=null;
+let f=Symbol("this is a symbol variable");
+let g=undefined;
+
+console.log(a,b,c,d,e,f,g);
